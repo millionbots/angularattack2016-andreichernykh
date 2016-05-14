e9d81998864194b30af4a9e12333249be79d9bc2
Andrei Chernykh <a.d.chernykh@gmail.com>
2016-05-14 18:34:11 +0300

    update field

--- src/app/components/tile/tile.component.ts
+++ src/app/components/tile/tile.component.ts
@@ -11,4 +11,27 @@ import {Tile, TileType} from "../../models/tile";
 })
 export class TileComponent {
   @Input() tile: Tile;
+
+  getTerrainClasses() {
+    let terrainType: string;
+    switch (this.tile.type) {
+      case TileType.Dirt: 
+        terrainType = 'terrain-dirt';
+        break;
+      case TileType.Grass:
+        terrainType = 'terrain-grass';
+        break;
+      case TileType.Road:
+        terrainType = 'terrain-road';
+        break;
+      case TileType.Rock:
+        terrainType = 'terrain-rock';
+        break;
+      case TileType.Water:
+        terrainType = 'terrain-water';
+        break;
+    }
+
+    return `terrain ${terrainType}`;
+  }
 }
